7aada18991976fbab8074ebe79401f1b373b89fc
Luiz Felipe Warmling Amadeu <85834483+Amad3eu@users.noreply.github.com>
2022-05-23 09:15:41 -0300

    add create form

---
+++ handlers/update.go
@@ -0,0 +1,46 @@
+package handlers
+
+import( 
+	"encoding/json"
+	"log"
+	"net/http"
+ 	"srtconv"
+
+ 	"github.com/aprendagolang/api-psql/models"
+)
+
+func Update(w http.ResponseWriter, r *http.request){
+	id, err := strconv.Atoi(chi.URLParam(r,"id"))
+	if err != nil{
+	log.println("Error decoding JSON: %v", err)
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	return
+}
+
+ var todo models.todo
+
+ err = json.NewDecoder(r.body).Decode(&todo)
+ if err != nil{
+ log.println("Error decoding JSON: %v", err)
+ http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+ return
+}
+
+rows, err := models.Update(int64(id,todo))
+	if err != nil{
+	log.println("Error ao atualizar registro: %v", err)
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	return
+} 
+
+if rows > 1{
+	log.printf("Error: foram atualizados %d registros", rows)
+}
+resp := map[string]any{
+	"Error":false
+	"Message":"dados atualizados com sucesso!",
+}
+
+w.header().Add("Content-Type", "application/json")
+json.NewEncoder(w).Encode(resp)
+}
