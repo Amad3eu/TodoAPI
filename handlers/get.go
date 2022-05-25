cf12f781f4a7ccbf6bf31e2d1d2ee6f272fdab17
Luiz Felipe Warmling Amadeu <85834483+Amad3eu@users.noreply.github.com>
2022-05-25 20:08:48 -0300

    add handlers files
    add methods "TODO"  necessary a revision (with errors)

--- handlers/get.go
+++ handlers/get.go
@@ -1,19 +1,15 @@
 package handlers
 
 
-import "net/http
+import ("net/http"
+ "strconv"
+"github.com/aprendagolang/api-pqsql/models"
+)
+
 func Get(w http.ResponseWritter, r *http.Request){
 id, err := strconv.Atoi(chi.URLParam(r,"id"))
 if err != nil{
 log.println("Erro ao fazer parse do id: %v", err)
-http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-return
-}
-
-
-err = json.NewDecoder(r.body).Decode(&todo)
-if err != nil{
-log.println("Error decoding JSON: %v", err)
 http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 return
 }
@@ -24,14 +20,6 @@ log.println("Error ao atualizar registro: %v", err)
 http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 return
 } 
-
-if rows > 1{
-log.printf("Error: foram atualizados %d registros", rows)
-}
-resp := map[string]any{
-"Error":false
-"Message":"dados atualizados com sucesso!",
-}
 
 w.header().Add("Content-Type", "application/json")
 json.NewEncoder(w).Encode(todo)
